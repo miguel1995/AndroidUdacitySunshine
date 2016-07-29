6ecd42f0d680c94ecd7460bb8c5b5f5458b79e64
miguel <galletaconmani@hotmail.com>
2016-07-28 23:40:17 -0500

    Initial commit

---
+++ app/src/main/java/com/example/miguel/sunshine/app/ForcastFragment.java
@@ -0,0 +1,76 @@
+package com.example.miguel.sunshine.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ForcastFragment extends Fragment {
+
+    public ForcastFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle saveInstaceState){
+        super.onCreate(saveInstaceState);
+        setHasOptionsMenu(true);
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if( id == R.id.action_refresh ){
+            FetchWatherTask watherTask = new FetchWatherTask();
+            watherTask.execute("94043");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/46",
+                "Weds - Cloudy - 72/63",
+                "Thurs - Rainy - 64/51",
+                "Fri - Foggy - 70/46",
+                "Sut - Sunny - 76/68" };
+        
+        List <String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+        ArrayAdapter<String> mForestAdapter = new ArrayAdapter<String>(
+                getContext(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+
+        listView.setAdapter(mForestAdapter);
+
+
+        return rootView;
+    }
+}
